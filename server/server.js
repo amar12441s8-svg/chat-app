3a4e27df110b4c4a802dedaf11823a2d3139451f
Amar <amar12441@gmail.com>
2026-03-03 21:38:07 +0530

    serve frontend

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ const cors = require("cors");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-
 app.use(cors());
 app.use(express.static("../client"));
 
@@ -83,6 +82,16 @@ io.on("connection", (socket) => {
 
 });
 
-server.listen(5000, () =>
-    console.log("Server running on port 5000")
-);
+const PORT = process.env.PORT || 5000;
+const path = require("path");
+
+// Serve static frontend files
+app.use(express.static(path.join(__dirname, "../client")));
+
+// Route for homepage
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/index.html"));
+});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
