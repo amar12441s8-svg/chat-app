9fd23851e265c822b4797ed920ebe31f4b09bb28
Amar <amar12441@gmail.com>
2026-03-04 16:38:27 +0530

    final server fix

--- server/server.js
+++ server/server.js
@@ -3,95 +3,114 @@ const http = require("http");
 const socketio = require("socket.io");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-app.use(cors());
-app.use(express.static("../client"));
 
-// MongoDB
-mongoose.connect("mongodb+srv://amar12441s8_db_user:WZEuHtxnJu8u7cD0@cluster0.gpgwuqb.mongodb.net/chatapp")
-.then(() => console.log("MongoDB Atlas Connected"))
-.catch(err => console.log(err));
+app.use(cors());
+
+/* ---------- Serve Frontend ---------- */
+app.use(express.static(path.join(__dirname, "../client")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/index.html"));
+});
+
+/* ---------- MongoDB ---------- */
+mongoose
+  .connect("mongodb+srv://amar12441s8_db_user:WZFUhXxnJu8u7cD0@cluster0.gpgwuqb.mongodb.net/chat")
+  .then(() => console.log("MongoDB Atlas Connected"))
+  .catch((err) => console.log(err));
+
+/* ---------- Schema ---------- */
 const messageSchema = new mongoose.Schema({
-    name: String,
-    text: String,
-    room: String,
-    time: { type: Date, default: Date.now }
+  name: String,
+  text: String,
+  room: String,
+  time: { type: Date, default: Date.now },
 });
 
 const Message = mongoose.model("Message", messageSchema);
 
+/* ---------- Users ---------- */
 let users = [];
 
+/* ---------- Socket Connection ---------- */
 io.on("connection", (socket) => {
 
-    socket.on("joinRoom", async ({ name, room }) => {
+  /* Join Room */
+  socket.on("joinRoom", async ({ name, room }) => {
 
-        socket.username = name;
-        socket.room = room;
+    socket.username = name;
+    socket.room = room;
 
-        socket.join(room);
-        users.push({ id: socket.id, name, room });
+    socket.join(room);
 
-        const oldMessages = await Message.find({ room });
-
-        oldMessages.forEach(msg => {
-            socket.emit("message", msg);
-        });
-
-        io.to(room).emit("roomUsers",
-            users.filter(u => u.room === room)
-        );
+    users.push({
+      id: socket.id,
+      name,
+      room
     });
 
-    socket.on("sendMessage", async ({ message }) => {
+    const oldMessages = await Message.find({ room });
 
-        const newMsg = new Message({
-            name: socket.username,
-            text: message,
-            room: socket.room
-        });
-
-        const savedMsg = await newMsg.save();
-
-        io.to(socket.room).emit("message", savedMsg);
+    oldMessages.forEach((msg) => {
+      socket.emit("message", msg);
     });
 
-    // 🔥 FIXED DELETE
-    socket.on("deleteMessage", async (id) => {
+    io.to(room).emit(
+      "roomUsers",
+      users.filter((u) => u.room === room)
+    );
 
-        if (!id) return;
+  });
 
-        const msg = await Message.findById(id);
+  /* Send Message */
+  socket.on("sendMessage", async ({ message }) => {
 
-        if (!msg) return;
-
-        // Only sender can delete
-        if (msg.name !== socket.username) return;
-
-        await Message.deleteOne({ _id: id });
-
-        io.to(socket.room).emit("messageDeleted", id.toString());
+    const newMsg = new Message({
+      name: socket.username,
+      text: message,
+      room: socket.room
     });
 
-    socket.on("disconnect", () => {
-        users = users.filter(u => u.id !== socket.id);
-    });
+    const savedMsg = await newMsg.save();
+
+    io.to(socket.room).emit("message", savedMsg);
+
+  });
+
+  /* Delete Message */
+  socket.on("deleteMessage", async (id) => {
+
+    if (!id) return;
+
+    const msg = await Message.findById(id);
+
+    if (!msg) return;
+
+    if (msg.name !== socket.username) return;
+
+    await Message.deleteOne({ _id: id });
+
+    io.to(socket.room).emit("messageDeleted", id.toString());
+
+  });
+
+  /* Disconnect */
+  socket.on("disconnect", () => {
+
+    users = users.filter((u) => u.id !== socket.id);
+
+  });
 
 });
 
+/* ---------- Server ---------- */
 const PORT = process.env.PORT || 5000;
-const path = require("path");
 
-// Serve static frontend files
-app.use(express.static(path.join(__dirname, "../client")));
-
-// Route for homepage
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/index.html"));
-});
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
